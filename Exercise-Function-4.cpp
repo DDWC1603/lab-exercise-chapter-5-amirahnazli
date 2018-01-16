0aa0d3b8e2b0ce073cc4166401a4c9681fff41b4
amirahnazli <33883963+amirahnazli@users.noreply.github.com>
2018-01-16 11:41:36 +0800

    task completed :)

--- Exercise-Function-4.cpp
+++ Exercise-Function-4.cpp
@@ -7,16 +7,25 @@
 
 using namespace std;
 
+void ombakOmbak(int, int, int, int);
+void OmbakKeluar();
+
+int i;
+
 void ombakOmbak(int repeat=3,int height=5,int up=2,int down=2)
 {
 	while(repeat >0)
 	{
 		for(int i=1; i<=up; i++)
-		cout<<setw(height)<<right<<"+"<<endl;
+		{
+			cout<<setw(height)<<right<<"+"<<endl;
+		}
+		
 		
 		for(int i=1; i<=down; i++)
-		cout<<"+"<<endl;
-		
+		{
+			cout<<"+"<<endl;
+		}
 		repeat --;
 	}
 
@@ -28,3 +37,8 @@ void OmbakKeluar()
 	cout<<"\nwaveDemo\n";
 	ombakOmbak();
 }
+ 
+ int main(void)
+ {
+ 	OmbakKeluar();
+ }
