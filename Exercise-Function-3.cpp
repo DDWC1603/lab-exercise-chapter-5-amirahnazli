a63290fd2237f30a7c4504124a7861f0d23edcaf
amirahnazli <33883963+amirahnazli@users.noreply.github.com>
2018-01-16 10:55:03 +0800

    task completed :)

--- Exercise-Function-3.cpp
+++ Exercise-Function-3.cpp
@@ -1,5 +1,12 @@
 //complete the program below
 //its a program to find the average value.
+//nur amirah afiqah
+//a17dw2172
+
+#include<iostream>
+using namespace std;
+
+double avg(double x,double y);
 
 int main()
 {
@@ -7,20 +14,23 @@ int main()
 	double n=0.0;
 
 	cout<<"Enter first number"<<endl;
+	cin>>m;
 	
 	cout<<"Enter second number"<<endl;
+	cin>>n;
 	
 	std::cin.get();
 
-	cout<<"Average is: "<</*function here*/<<endl;
+	cout<<"Average is: "<< avg(m,n) <<endl;
 	
-	
+	return 0;
 	
 }
 
 double avg(double x,double y)
 {
 	/*return the average value of x and y*/
-
+	
+	return (x+y)/2;
 	
 }
